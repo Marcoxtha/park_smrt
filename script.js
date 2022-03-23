a1e222f3e20cc21010b500c0f298e0e44eb6d82e
rohann12 <70807244+rohann12@users.noreply.github.com>
2022-03-23 19:36:30 +0545

    Add files via upload
    yeti gare maile

---
+++ script.js
@@ -0,0 +1,26 @@
+const bike = document.querySelectorAll('.bike');
+
+bike.forEach(el => el.addEventListener('click', event => {
+    alert("Bike");
+    alert(event.target.getAttribute("id"));
+    console.log(event.target.getAttribute("id"))
+    var x = event.target.getAttribute("id");
+    document.getElementById("lform").style.display = "block";
+    document.getElementsByClassName('main-div')[0].style.display = 'none';
+
+
+}));
+
+
+const car = document.querySelectorAll('.car');
+
+car.forEach(el => el.addEventListener('click', event => {
+    alert("Car");
+    alert(event.target.getAttribute("id"));
+    console.log(event.target.getAttribute("id"));
+}));
+
+function closeForm() {
+    document.getElementById("lform").style.display = "none";
+    document.getElementsByClassName("main-div")[0].style.display = "block";
+}
